598313c4d4c6d1280ed21ecbb56bd0fe818d1b08
Geoffroy Couprie <geo.couprie@gmail.com>
2012-03-26 18:20:47 +0200

    add the skeleton for a JS module, with a cross platform getElementByClassName function

---
+++ ppsb.js
@@ -0,0 +1,46 @@
+/*  
+ * ppsb.js: Privacy preserving social buttons
+ *
+ * Copyright 2012 Geoffroy Couprie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+(function () {
+	function mygetElementsByClassName(classname) {
+		if (document.getElementsByClassName)
+			return document.getElementsByClassName(classname);
+		else {
+			var classElements = new Array();
+			var els = document.getElementsByTagName("*");
+			var elsLen = els.length;
+			var pattern = new RegExp("(^|\\s)"+classname+"(\\s|$)");
+			for (i = 0, j = 0; i < elsLen; i++) {
+				if ( pattern.test(els[i].className) ) {
+					classElements[j] = els[i];
+					j++;
+				}
+			}
+			return classElements;
+		}
+	}
+}());
+
+
