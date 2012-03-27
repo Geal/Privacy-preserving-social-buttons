a070d0ded7eb3bfa2189f058fbbacd3979d65fc9
Geoffroy Couprie <geo.couprie@gmail.com>
2012-03-27 16:57:13 +0200

    factor the handler code

--- ppsb.js
+++ ppsb.js
@@ -1,4 +1,4 @@
-/*  
+/*
  * ppsb.js: Privacy preserving social buttons
  *
  * Copyright 2012 Geoffroy Couprie
@@ -42,53 +42,24 @@
         }
     }
 
-    var twitterNodes = mygetElementsByClassName("pp-twitter");
-    for(var i = 0; i< twitterNodes.length; i++){
-        var node = twitterNodes[i];
-        var dataUrl = node.attributes["data-url"].value;
-        var dataVia = node.attributes["data-via"].value;
-        var dataText = node.attributes["data-text"].value;
-        node.onclick = function(){
-        var url = "https://twitter.com/intent/tweet?url="+dataUrl+"&text="+dataText+"&via="+dataVia;
-        var windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes';
-        var width = 550;
-        var height = 420;
-        window.open(url, 'intent', windowOptions + ',width=' + width +
-                                           ',height=' + height);// + ',left=' + left + ',top=' + top);
-        };
-        console.log(node.attributes["data-url"].value);
-    };
+    function addHandler(cssClass, urlCreator) {
+        var nodes = mygetElementsByClassName(cssClass);
+        for(var i = 0; i< nodes.length; i++){
+            var node = nodes[i];
+            var dataUrl = node.attributes["data-url"].value;
+            var dataVia = node.attributes["data-via"].value;
+            var dataText = node.attributes["data-text"].value;
+            node.onclick = function(){
+            var url = urlCreator(dataUrl, dataVia, dataText);
+            var windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes';
+            var width = 550;
+            var height = 420;
+            window.open(url, 'intent', windowOptions + ',width=' + width +
+                            ',height=' + height);
+            };
+        }
+    }
 
-    var redditNodes = mygetElementsByClassName("pp-reddit");
-    for(var i = 0; i< redditNodes.length; i++){
-        var node = redditNodes[i];
-        var dataUrl = node.attributes["data-url"].value;
-        var dataVia = node.attributes["data-via"].value;
-        var dataText = node.attributes["data-text"].value;
-        node.onclick = function(){
-        var url = "http://www.reddit.com/submit?url="+dataUrl+"&title="+dataText;
-        var windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes';
-        var width = 550;
-        var height = 420;
-        window.open(url, 'intent', windowOptions + ',width=' + width +
-                                           ',height=' + height);// + ',left=' + left + ',top=' + top);
-        };
-        console.log(node.attributes["data-url"].value);
-    };
-
-    var facebookNodes = mygetElementsByClassName("pp-facebook");
-    for(var i = 0; i< facebookNodes.length; i++){
-        var node = facebookNodes[i];
-        var dataUrl = node.attributes["data-url"].value;
-        var dataVia = node.attributes["data-via"].value;
-        var dataText = node.attributes["data-text"].value;
-        node.onclick = function(){
-        var url = "https://www.facebook.com/sharer.php?u="+dataUrl+"&t="+dataText;
-        var windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes';
-        var width = 550;
-        var height = 420;
-        window.open(url, 'intent', windowOptions + ',width=' + width +
-                                           ',height=' + height);// + ',left=' + left + ',top=' + top);
-        };
-        console.log(node.attributes["data-url"].value);
-    };
+    addHandler("pp-twitter",  function(url, via, text) { return "https://twitter.com/intent/tweet?url="+url+"&text="+text+"&via="+via } );
+    addHandler("pp-facebook", function(url, via, text) { return "https://www.facebook.com/sharer.php?u="+url+"&t="+text } );
+    addHandler("pp-reddit",   function(url, via, text) { return "http://www.reddit.com/submit?url="+url+"&title="+text } );
