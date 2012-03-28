0f52a6a227f664229f0f0e06615501c9b37d7dab
Geoffroy Couprie <geo.couprie@gmail.com>
2012-03-28 17:05:23 +0200

    include the social buttons in the page

---
+++ ppsb.js
@@ -0,0 +1,77 @@
+/*
+ * ppsb.js: Privacy preserving social buttons
+ *
+ * Copyright 2012 Geoffroy Couprie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+//(function () {
+    function mygetElementsByClassName(classname) {
+        if (document.getElementsByClassName)
+            return document.getElementsByClassName(classname);
+        else {
+            var classElements = new Array();
+            var els = document.getElementsByTagName("*");
+            var elsLen = els.length;
+            var pattern = new RegExp("(^|\\s)"+classname+"(\\s|$)");
+            for (i = 0, j = 0; i < elsLen; i++) {
+                if ( pattern.test(els[i].className) ) {
+                    classElements[j] = els[i];
+                    j++;
+                }
+            }
+            return classElements;
+        }
+    }
+
+    function addHandler(cssClass, urlCreator) {
+        var windowOptions = 'scrollbars=yes,resizable=yes,toolbar=no,location=yes';
+        var width = 550;
+        var height = 420;
+        var winHeight = screen.height;
+        var winWidth = screen.width;
+
+        left = Math.round((winWidth / 2) - (width / 2));
+        top = 0;
+
+        if (winHeight > height) {
+            top = Math.round((winHeight / 2) - (height / 2));
+        }
+
+        var nodes = mygetElementsByClassName(cssClass);
+        for(var i = 0; i< nodes.length; i++){
+            var node = nodes[i];
+            var dataUrl = node.attributes["data-url"].value;
+            var dataVia = node.attributes["data-via"].value;
+            var dataText = node.attributes["data-text"].value;
+            node.onclick = function(){
+            var url = urlCreator(dataUrl, dataVia, dataText);
+
+            window.open(url, 'intent', windowOptions + ',width=' + width +
+                            ',height=' + height+ ',left=' + left + ',top=' + top);
+            };
+        }
+    }
+
+    addHandler("pp-twitter",  function(url, via, text) { return "https://twitter.com/intent/tweet?url="+url+"&text="+text+"&via="+via } );
+    addHandler("pp-facebook", function(url, via, text) { return "https://www.facebook.com/sharer.php?u="+url+"&t="+text } );
+    addHandler("pp-reddit",   function(url, via, text) { return "http://www.reddit.com/submit?url="+url+"&title="+text } );
+    addHandler("pp-gplus",    function(url, via, text) { return "https://plusone.google.com/_/+1/confirm?hl=en-US&url="+url+"&source=widget&referer="+url } );
